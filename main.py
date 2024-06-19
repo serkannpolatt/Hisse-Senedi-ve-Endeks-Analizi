03257b1acd0ed2d3b2ba2f63ed937599b888e9f9
Serkan Polat <itsonlydatahustle@gmail.com>
2024-06-19 14:08:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from tvDatafeed import TvDatafeed, Interval
 import pandas as pd
 import numpy as np
 import streamlit as st
-
+import numpy as np
+import pandas as pd
+import pandas_ta as ta
+from tvDatafeed import TvDatafeed, Interval
+import streamlit as st
+import ssl
+from urllib import request
 #XBANK	#BIST BANKA
 #XBLSM	#BIST BİLİŞİM
 #XELKT	#BIST ELEKTRİK
